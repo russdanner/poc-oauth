d1a9af4922ae654211b92834c19824798f299390
admin admin <evaladmin@example.com>
2020-06-09 12:12:49 -0400

    User admin wrote content /scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy

--- scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
+++ scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
@@ -5,4 +5,19 @@ package org.craftercms.oauth
 
 public class CrafterOAuth {
     
+    public doSecurityFilter(request, response, session) {
+       // final Config config = getSharedConfig();
+
+       // final SessionStore<JEEContext> bestSessionStore = FindBest.sessionStore(null, config, JEESessionStore.INSTANCE);
+        final HttpActionAdapter<Object, JEEContext> bestAdapter = FindBest.httpActionAdapter(null, config, JEEHttpActionAdapter.INSTANCE);
+        final SecurityLogic<Object, JEEContext> bestLogic = FindBest.securityLogic(securityLogic, config, DefaultSecurityLogic.INSTANCE);
+
+        final JEEContext context = new JEEContext(request, response, bestSessionStore);
+        bestLogic.perform(context, config, (ctx, profiles, parameters) -> {
+            // if no profiles are loaded, pac4j is not concerned with this request
+            filterChain.doFilter(profiles.isEmpty() ? request : new Pac4JHttpServletRequestWrapper(request, profiles), response);
+            return null;
+        }, bestAdapter, clients, authorizers, matchers, multiProfile);
+    }
+    }
 }
