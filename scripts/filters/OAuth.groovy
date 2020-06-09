67058fe982c0716fb897145271cf3ba35534bfae
admin admin <evaladmin@example.com>
2020-06-09 11:45:14 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,21 +1,21 @@
-import org.craftercms.oauth.CrafterOAuth
+@Grab(group='io.corbel', module='oauth-filter-api', version='1.44.0')
 
-import io.corbel.lib.token.TokenGrant;
-import io.corbel.lib.token.TokenInfo;
-import io.corbel.lib.token.factory.TokenFactory;
-import io.corbel.lib.token.model.TokenType;
-import io.corbel.lib.token.parser.TokenParser;
-import io.corbel.lib.token.provider.SessionProvider;
-import io.corbel.oauth.filter.FilterRegistry;
-import io.corbel.oauth.filter.exception.AuthFilterException;
-import io.corbel.oauth.model.Client;
-import io.corbel.oauth.model.User;
-import io.corbel.oauth.service.ClientService;
-import io.corbel.oauth.service.UserService;
-import io.corbel.oauth.session.SessionBuilder;
-import io.corbel.oauth.session.SessionCookieFactory;
-import io.corbel.oauth.token.TokenExpireTime;
-import io.dropwizard.testing.junit.ResourceTestRule;
+import io.corbel.lib.token.TokenGrant
+import io.corbel.lib.token.TokenInfo
+import io.corbel.lib.token.factory.TokenFactory
+import io.corbel.lib.token.model.TokenType
+import io.corbel.lib.token.parser.TokenParser
+import io.corbel.lib.token.provider.SessionProvider
+import io.corbel.oauth.filter.FilterRegistry
+import io.corbel.oauth.filter.exception.AuthFilterException
+import io.corbel.oauth.model.Client
+import io.corbel.oauth.model.User
+import io.corbel.oauth.service.ClientService
+import io.corbel.oauth.service.UserService
+import io.corbel.oauth.session.SessionBuilder
+import io.corbel.oauth.session.SessionCookieFactory
+import io.corbel.oauth.token.TokenExpireTime
+import io.dropwizard.testing.junit.ResourceTestRule
 
 
 logger.info("before")
