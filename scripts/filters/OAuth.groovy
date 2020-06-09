3704e1ea623c6fbb2496aa074d2c05830ab44ca9
admin admin <evaladmin@example.com>
2020-06-09 12:21:09 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,6 +1,6 @@
 @Grab(group='org.pac4j', module='pac4j-oauth', version='4.0.2')
 
-import org.craftercms.oauth.CrafterOAuth
+//import org.craftercms.oauth.CrafterOAuth
 import org.pac4j.core.util.FindBest
 
 logger.info("before")
