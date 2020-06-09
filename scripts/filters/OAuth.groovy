f2261901cec4d37555e8786fa5bd10037abebae1
admin admin <evaladmin@example.com>
2020-06-09 11:24:20 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,4 +1,8 @@
-//@Grab(group='se.curity', module='oauth-filter', version='2.0.0', initClass=false)
+// https://mvnrepository.com/artifact/se.curity/oauth-filter
+@Grapes(
+    @Grab(group='se.curity', module='oauth-filter', version='2.0.0')
+)
+
 
 logger.info("before")
 
