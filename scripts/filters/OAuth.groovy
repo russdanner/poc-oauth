4b6eb22d52853318146cfc1d42f464322b155444
admin admin <evaladmin@example.com>
2020-06-09 12:41:08 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,17 +1,18 @@
 import org.craftercms.oauth.CrafterOAuth
 
 def requestURI = request.getRequestURL()
+def oauth = new CrafterOAuth() // for now
 
 if("/callback".equals(requestURI)) {
     // if callback
-    CrafterOAuth.doCallbackFilter(request, response, session, filterChain)
+    oauth.doCallbackFilter(request, response, session, filterChain)
 }
 else if("/logout".equals(requestURI)) {
     // if logout
-    CrafterOAuth.doLogoutFilter(request, response, session, filterChain)
+    oauth.doLogoutFilter(request, response, session, filterChain)
 }
 else {
     // otherwise (filter any request passed our way)
-    CrafterOAuth.doSecurityFilter(request, response, session, filterChain)
+    oauth.doSecurityFilter(request, response, session, filterChain)
 }
         
