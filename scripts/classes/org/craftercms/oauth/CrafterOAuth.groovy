79288513a678584ae95b50dfb3de51ea7d89c531
admin admin <evaladmin@example.com>
2020-06-09 11:26:17 -0400

    User admin wrote content /scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy

---
+++ scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
@@ -0,0 +1,8 @@
+// https://mvnrepository.com/artifact/se.curity/oauth-filter
+@Grapes(
+    @Grab(group='se.curity', module='oauth-filter', version='2.0.0')
+)
+
+public class CrafterOAuth {
+    
+}
