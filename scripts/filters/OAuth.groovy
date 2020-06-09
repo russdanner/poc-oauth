556d261eb63e07a2e4aa44add7b57b7c1822d7ee
admin admin <evaladmin@example.com>
2020-06-09 11:58:39 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,22 +1,4 @@
-@Grab(group='io.corbel.lib', module='config', version='0.4.0')
-@Grab(group='io.corbel', module='oauth-filter-api', version='1.44.0')
-
-// import io.corbel.lib.token.TokenGrant
-// import io.corbel.lib.token.TokenInfo
-// import io.corbel.lib.token.factory.TokenFactory
-// import io.corbel.lib.token.model.TokenType
-// import io.corbel.lib.token.parser.TokenParser
-// import io.corbel.lib.token.provider.SessionProvider
-import io.corbel.oauth.filter.FilterRegistry
-import io.corbel.oauth.filter.exception.AuthFilterException
-import io.corbel.oauth.model.Client
-import io.corbel.oauth.model.User
-import io.corbel.oauth.service.ClientService
-import io.corbel.oauth.service.UserService
-import io.corbel.oauth.session.SessionBuilder
-import io.corbel.oauth.session.SessionCookieFactory
-import io.corbel.oauth.token.TokenExpireTime
-
+@Grab(group='org.pac4j', module='pac4j-oauth', version='4.0.2', scope='runtime')
 
 
 logger.info("before")
