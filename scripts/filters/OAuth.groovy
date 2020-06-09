0ae1bcc2b5dbafa08bed4677fe3fa817d2b6d6d0
admin admin <evaladmin@example.com>
2020-06-09 11:52:46 -0400

    User admin wrote content /scripts/filters/OAuth.groovy

--- scripts/filters/OAuth.groovy
+++ scripts/filters/OAuth.groovy
@@ -1,12 +1,12 @@
 @Grab(group='io.corbel.lib', module='config', version='0.4.0')
 @Grab(group='io.corbel', module='oauth-filter-api', version='1.44.0')
 
-import io.corbel.lib.token.TokenGrant
-import io.corbel.lib.token.TokenInfo
-import io.corbel.lib.token.factory.TokenFactory
-import io.corbel.lib.token.model.TokenType
-import io.corbel.lib.token.parser.TokenParser
-import io.corbel.lib.token.provider.SessionProvider
+// import io.corbel.lib.token.TokenGrant
+// import io.corbel.lib.token.TokenInfo
+// import io.corbel.lib.token.factory.TokenFactory
+// import io.corbel.lib.token.model.TokenType
+// import io.corbel.lib.token.parser.TokenParser
+// import io.corbel.lib.token.provider.SessionProvider
 import io.corbel.oauth.filter.FilterRegistry
 import io.corbel.oauth.filter.exception.AuthFilterException
 import io.corbel.oauth.model.Client
@@ -16,7 +16,6 @@ import io.corbel.oauth.service.UserService
 import io.corbel.oauth.session.SessionBuilder
 import io.corbel.oauth.session.SessionCookieFactory
 import io.corbel.oauth.token.TokenExpireTime
-import io.dropwizard.testing.junit.ResourceTestRule
 
 
 
