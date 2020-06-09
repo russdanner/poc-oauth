fae25231db1b2a303c428d86630ad687f41d2657
admin admin <evaladmin@example.com>
2020-06-09 12:59:28 -0400

    User admin wrote content /scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy

--- scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
+++ scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
@@ -58,17 +58,16 @@ public class CrafterOAuth {
        // final Config config = getSharedConfig();
 
        // final SessionStore<JEEContext> bestSessionStore = FindBest.sessionStore(null, config, JEESessionStore.INSTANCE);
-        final HttpActionAdapter<Object, JEEContext> bestAdapter = FindBest.httpActionAdapter(null, config, JEEHttpActionAdapter.INSTANCE);
-        final SecurityLogic<Object, JEEContext> bestLogic = FindBest.securityLogic(securityLogic, config, DefaultSecurityLogic.INSTANCE);
+        // final HttpActionAdapter<Object, JEEContext> bestAdapter = FindBest.httpActionAdapter(null, config, JEEHttpActionAdapter.INSTANCE);
+        // final SecurityLogic<Object, JEEContext> bestLogic = FindBest.securityLogic(securityLogic, config, DefaultSecurityLogic.INSTANCE);
 
-        final JEEContext context = new JEEContext(request, response, bestSessionStore);
-        bestLogic.perform(context, config, (ctx, profiles, parameters) -> {
-            // if no profiles are loaded, pac4j is not concerned with this request
-            filterChain.doFilter(profiles.isEmpty() ? request : , response);
-            return null;
-        }, bestAdapter, clients, authorizers, matchers, multiProfile);
-    }
-    */
+        // final JEEContext context = new JEEContext(request, response, bestSessionStore);
+        // bestLogic.perform(context, config, (ctx, profiles, parameters) -> {
+        //     // if no profiles are loaded, pac4j is not concerned with this request
+        //     filterChain.doFilter(profiles.isEmpty() ? request : , response);
+        //     return null;
+        // }, bestAdapter, clients, authorizers, matchers, multiProfile);
+//    }
     
     }
 
