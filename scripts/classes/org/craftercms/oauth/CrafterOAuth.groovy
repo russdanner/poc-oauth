1c71e801f721cd305246aadbfe8fb9f3f5d0ad65
admin admin <evaladmin@example.com>
2020-06-09 12:06:52 -0400

    User admin wrote content /scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy

--- scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
+++ scripts/classes/org/craftercms/oauth/CrafterOAuth.groovy
@@ -1,6 +1,7 @@
 package org.craftercms.oauth
 
-@Grab(group='io.corbel', module='oauth-filter-api', version='1.44.0')
+//@Grab(group='io.corbel', module='oauth-filter-api', version='1.44.0')
+@Grab(group='org.pac4j', module='pac4j-oauth', version='4.0.2')
 
 public class CrafterOAuth {
     
